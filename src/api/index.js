d045c5279560c2aac7a0a8fcce72a095e2ab296c
Masho Ado <mashoado@gmail.com>
2023-02-27 22:33:37 +0300

    refactor api endpoints

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import serverCheck from "./server-check";
-import { products } from "./storefront/products";
+import { product, products } from "./storefront/products";
+import cors from "cors";
+import { projectConfig } from "../../../medusa-config";
+
+const storeCorsOptions = {
+  origin: projectConfig.store_cors,
+  credentials: true,
+};
 
 export default () => {
   const router = Router();
+  router.options("store/products", storeCorsOptions);
 
   serverCheck(router);
   products(router);
+  product(router);
 
   return router;
 };
--- src/api/storefront/products.js
+++ src/api/storefront/products.js
@@ -1,9 +1,16 @@
 export const products = (router) => {
-  router.get("/storefront/products", async (req, res) => {
+  router.get("/store/products", async (req, res) => {
     const productService = req.scope.resolve("productService");
 
-    res.json({
-      products: await productService.list({}, {}),
-    });
+    res.json(await productService.list({}, {}));
+  });
+};
+
+export const product = (router) => {
+  router.get("/store/products/:id", async (req, res) => {
+    const productService = req.scope.resolve("productService");
+    const { id } = req.params;
+
+    res.json(await productService.retrieve(id));
   });
 };
