a75bcede0a61fb101b1bd68c84db6130aefd5c57
Masho Ado <mashoado@gmail.com>
2023-02-28 10:04:37 +0300

    remove remaining cors configuration

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,6 @@ import { Router } from "express";
 import serverCheck from "./server-check";
 import { product, products } from "./storefront/products";
 
-const storeCorsOptions = {
-  origin: projectConfig.store_cors,
-  credentials: true,
-};
-
 export default () => {
   const router = Router();
 
